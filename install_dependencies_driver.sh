4cc1d76d16d5dbb4a2cf9b3b69792593f74cfe8b
korchiev <nkorchi@ncsu.edu>
2022-09-24 22:52:41 -0400

    updated needed dependency list

--- install_dependencies_driver.sh
+++ install_dependencies_driver.sh
@@ -1,3 +1,28 @@
+#Install Docker
+MYUSER=`whoami`
+sudo apt-get update
+sudo apt-get install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io
+sudo adduser $MYUSER docker
+
+#install Stardog
+sudo docker pull stardog/stardog:latest
+
+#create directory ~/stardog-home and put the license key inside this folder. 
+#stardog is needed to get manufacturing ontologies for the experiments
+#then run
+#sudo docker run -it -v ~/stardog-home/:/var/opt/stardog -p 5820:5820 stardog/stardog
+#sudo docker run --name=stardog-studio -p 8888:8080 -d stardog/stardog-studio:current
+
 
 #Install SDK to install Gradle, run the following commands: 
 curl -s "https://get.sdkman.io" | bash
